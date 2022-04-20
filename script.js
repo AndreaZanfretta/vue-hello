61cc2c2781cc5bd372663ae63b679964dac3ca06
AndreaZanfretta <andrea.zanfretta.97@gmail.com>
2022-04-20 13:49:57 +0200

    esercizio finito

---
+++ script.js
@@ -0,0 +1,10 @@
+const app = new Vue({
+    el: '#root',
+    data: {
+      messaggio: "Hello World Vue!",
+      immagine: "https://picsum.photos/id/1015/900/800"
+    },
+    methods: {
+       
+    }
+  })
