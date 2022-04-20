1254217e17dfd9600e933214600596802f366995
AndreaZanfretta <andrea.zanfretta.97@gmail.com>
2022-04-20 16:04:27 +0200

    fix vari

--- script.js
+++ script.js
@@ -2,9 +2,26 @@ const app = new Vue({
     el: '#root',
     data: {
       messaggio: "Hello World Vue!",
-      immagine: "https://picsum.photos/id/1015/900/800"
+      immagine: "https://picsum.photos/id/",
+      imgID: "1",
+      imgSize: "/900/800",
+      effect: " "
     },
     methods: {
-       
+       nextImmagine(){
+            if(this.imgID<20){
+                this.imgID++;
+                
+            }else{
+                this.imgID = 1;
+            }
+       },
+       prevImmagine(){
+        if(this.imgID<2){
+            this.imgID = 20;
+        }else{
+            this.imgID--;
+        };
+    }
     }
   })
